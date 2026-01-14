0c2c7e71018599f8f81da15c58f5c22896a2ec74
Daniel Hoherd <daniel.hoherd@gmail.com>
2026-01-14 10:18:19 -0500

    Simplify release. We don't care about releasing from a tag.

--- apc-scripts/astronomer-airflow-chart-release.sh
+++ apc-scripts/astronomer-airflow-chart-release.sh
@@ -1,51 +1,26 @@
 #!/usr/bin/env bash
 
-# This file is Not licensed to ASF
-# SKIP LICENSE INSERTION
-
-ROOT_DIRECTORY=$(pwd)
+ROOT_DIRECTORY="$PWD"
 CHART_DIRECTORY=${ROOT_DIRECTORY}/chart
 CHART_RELEASE_DIRECTORY=${ROOT_DIRECTORY}/chart-rel
-export ROOT_DIRECTORY CHART_DIRECTORY CHART_RELEASE_DIRECTORY
 
+# Report and exit if there is no chart directory
 if [[ ! -d "$CHART_DIRECTORY" ]]; then
-  echo "chart directory does not exists ..."
+  echo "ERROR: Directory ${CHART_DIRECTORY} does not exist..."
   echo
-  echo "Current Working Directory: "
+  echo "Root directory contents:"
   echo
-  ls -ltr
+  ls -ltr "${ROOT_DIRECTORY}"
+  exit 1
 fi
 
 CHART_VERSION="$(awk '$1 ~ /^version/ {printf $2;exit;}' "$CHART_DIRECTORY"/Chart.yaml)"
-export CHART_VERSION
-export CHART_GH_RELEASE_TAG="oss-helm-chart/$CHART_VERSION"
-export CHART_PUBLISH_REPO="https://github.com/astronomer/airflow/releases/download/${CHART_GH_RELEASE_TAG}"
 
-# shellcheck disable=SC2153
-export TAG=${GITHUB_REF/refs\/tags\//}
-if [[ "$GITHUB_REF" != *"tags"* ]]; then
-  export TAG=""
-fi
-echo "TAG: $TAG"
-# shellcheck disable=SC2153
-export BRANCH=${GITHUB_REF#refs/*/}
-echo "BRANCH: $BRANCH"
-
-
-function package_chart() {
-    echo "Packaging chart ..."
-    helm package chart --dependency-update "$CHART_DIRECTORY" --destination "$CHART_RELEASE_DIRECTORY"
-}
-
-# If the build is not a tag/release, build a dev version
-if [[ -z ${TAG:=} ]]; then
-    echo
-    echo "Current Chart Version is: ${CHART_VERSION}"
-    echo
-    package_chart
-elif [[ ${TAG:=} == "${BRANCH:=}" ]]; then
-    echo
-    # TODO: Release the chart
-fi
+echo
+echo "Current Chart Version is: ${CHART_VERSION}"
+echo
+echo "Packaging chart ..."
+echo
+helm package chart --dependency-update "$CHART_DIRECTORY" --destination "$CHART_RELEASE_DIRECTORY"
 
 ls -altr "${CHART_RELEASE_DIRECTORY}/"
